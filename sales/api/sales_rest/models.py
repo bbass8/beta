49afa21b4abcb4c3ad5cbe09275d29960d0a6887
Ben <benbass8@gmail.com>
2023-04-24 14:59:18 -0400

    ReadMe

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
